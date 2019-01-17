624eb25ef74b70e68aff6ac52487738ade716945
Christophe Romain <christophe.romain@process-one.net>
2019-01-17 11:21:43 +0100

    Use alpine:edge to use OTP 21.2

--- ecs/build.sh
+++ ecs/build.sh
@@ -4,4 +4,4 @@ current=$(date +%y.%m)
 version=${1:-$current}
 
 docker build --build-arg VERSION=$version -t ejabberd/ecs:$version .
-docker tag ejabberd/ecs:$version ejabberd/ecs:latest
+[ "$version" = "latest" ] || docker tag ejabberd/ecs:$version ejabberd/ecs:latest
